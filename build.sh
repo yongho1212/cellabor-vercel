16a2099f4a2ffb0f66194bcf11ae4b231b726f7f
yongho1212 <yongho6800@gmail.com>
2024-06-05 09:46:14 +0000

    lint

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
+
 #!/bin/sh
 cd ../
 mkdir output
 cp -R ./cellabor-client/* ./output
 cp -R ./output ./cellabor-client/
+
+
+##!/bin/sh
+#cd ../
+#mkdir output
+#rsync -av ./cellabor-client/ ./output/
+#rsync -av ./output/ ./cellabor-client/
